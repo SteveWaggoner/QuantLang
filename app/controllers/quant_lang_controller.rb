3926f5371e0efeb4d493e886be4478b1159b00bf
Ubuntu <ubuntu@ip-172-31-7-218.ec2.internal>
2018-03-11 07:38:21 +0000

    Add test form with POST method

---
+++ app/controllers/quant_lang_controller.rb
@@ -0,0 +1,8 @@
+class QuantLangController < ApplicationController
+  def index
+  end
+
+  def edit
+	render :edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'hello_world#index'
   get 'hello_world/index'
 
-  root 'hello_world#index'
+
+  match '/edit' => 'quant_lang#edit', via: :get
+  match '/edit' => 'quant_lang#edit', via: :post
+
 end
